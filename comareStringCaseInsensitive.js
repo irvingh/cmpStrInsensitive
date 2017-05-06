65c36991191abacc40b31bf8a445937dba01c0f4
Irving <irving.hornic@gmail.com>
2017-05-06 10:31:23 +0300

    compareStringCaseInsensitive

---
+++ comareStringCaseInsensitive.js
@@ -0,0 +1,4 @@
+function compareStringCaseInsensitive(a, b) {
+    for (var i=0, l=Math.min(a.length, b.length); i<l && a[i].toLowerCase() === b[i].toLowerCase(); ++i);
+    return (i == l) ? (a.length - b.length) : (a[i].toLowerCase().charCodeAt(0) - b[i].toLowerCase().charCodeAt(0));
+}
