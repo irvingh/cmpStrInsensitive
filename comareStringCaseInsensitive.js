bf0f6fc9ba5676c942db6ebb2d121e5b5cb6af29
Irving <irvingh@users.noreply.github.com>
2017-05-30 06:44:01 +0300

    Delete comareStringCaseInsensitive.js

--- comareStringCaseInsensitive.js
+++
@@ -1,4 +0,0 @@
-function cmpStrCaseInsensitive(a, b) {
-    for (var i=0, l=Math.min(a.length, b.length); i<l && a[i].toLowerCase() === b[i].toLowerCase(); ++i);
-    return (i == l) ? (a.length - b.length) : (a[i].toLowerCase().charCodeAt(0) - b[i].toLowerCase().charCodeAt(0));
-}
